75e580b63179b7192e1376ff66771db1dbc7de5b
michalnand <michal.nand@gmail.com>
2021-02-09 21:17:49 +0100

    reward scaling

--- RLAgents/lib_common/WrapperAtari.py
+++ RLAgents/lib_common/WrapperAtari.py
@@ -177,12 +177,16 @@ class EpisodicLifeEnv(gym.Wrapper):
         return obs
 
 class ClipRewardEnv(gym.Wrapper):
-    def __init__(self, env):
+    def __init__(self, env, scale=1.0):
         gym.Wrapper.__init__(self, env)
+
+        self.scale = scale
 
         self.raw_episodes            = 0
         self.raw_score_per_episode   = 0.0
         self.raw_score_total         = 0.0
+
+        
        
     def step(self, action):
         obs, reward, done, info = self.env.step(action)
@@ -191,22 +195,20 @@ class ClipRewardEnv(gym.Wrapper):
         self.raw_score_per_episode  = self.env.raw_score_per_episode
         self.raw_score_total        = self.env.raw_score_total
 
-        reward = numpy.clip(reward, -1.0, 1.0)
+        reward = numpy.clip(reward*self.scale, -1.0, 1.0)
 
         return obs, reward, done, info
 
 
-def WrapperAtari(env, height = 96, width = 96, frame_stacking=4, frame_skipping=4, max_steps = 4000):
+def WrapperAtari(env, height = 96, width = 96, frame_stacking=4, frame_skipping=4, max_steps = 4000, reward_scale = 0.01):
     env = NopOpsEnv(env)
     env = FireResetEnv(env) 
     env = MaxAndSkipEnv(env, frame_skipping)
     env = ResizeEnv(env, height, width, frame_stacking)
     env = MaxStepsEnv(env, max_steps)
     env = EpisodicLifeEnv(env)
-    env = ClipRewardEnv(env)
+    env = ClipRewardEnv(env, reward_scale)
 
     env.observation_space.shape = (frame_stacking, height, width)
 
     return env
-
-
