03aa0626764cd50421466e7eae3822c0f887d91c
michalnand <michal.nand@gmail.com>
2021-02-23 19:54:14 +0100

    common

--- RLAgents/lib_agents/AgentPPOEntropy.py
+++ RLAgents/lib_agents/AgentPPOEntropy.py
@@ -134,7 +134,7 @@ class AgentPPOEntropy():
         self.int_curiosity_reward_running_stats.update(curiosity_np)
         curiosity_np        = (curiosity_np - self.int_curiosity_reward_running_stats.mean)/self.int_curiosity_reward_running_stats.std
 
-        self._add_episodic_memory(states_np)
+        self._add_episodic_memory(states_t)
 
         entropy_np          = self._entropy()
         self.int_entropy_reward_running_stats.update(entropy_np)
