51794ad328a0b75f6682d579c8e7822953e89684
michalnand <michal.nand@gmail.com>
2021-03-15 20:31:45 +0100

    common

--- RLAgents/lib_common/WrapperAtari.py
+++ RLAgents/lib_common/WrapperAtari.py
@@ -169,7 +169,7 @@ class SparseEnv(gym.Wrapper):
         if reward < 0.0:
             reward_sparse = reward
         elif self.steps%self.sparsity_steps == 0:
-            reward_sparse   = self.reward_sum/self.sparsity_steps
+            reward_sparse   = self.reward_sum
             self.steps      = 0
             self.reward_sum = 0
 
@@ -197,6 +197,6 @@ def WrapperAtari(env, height = 96, width = 96, frame_stacking=4, frame_skipping=
 
 def WrapperAtariSparseRewards(env, height = 96, width = 96, frame_stacking=4, frame_skipping=4):
     env = WrapperAtari(env, height, width, frame_stacking, frame_skipping)
-    env = SparseEnv(env, sparsity_steps=30) 
+    env = SparseEnv(env, sparsity_steps=40) 
 
     return env
