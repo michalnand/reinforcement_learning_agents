7b862dc27aab38f27cd342977076b3185193b111
michalnand <michal.nand@gmail.com>
2021-03-30 14:10:05 +0200

    rnd + entropy agent

--- RLAgents/lib_agents/AgentPPOEntropy.py
+++ RLAgents/lib_agents/AgentPPOEntropy.py
@@ -100,7 +100,7 @@ class AgentPPOEntropy():
 
         #entropy motivation 
         entropy_np          = self._entropy(states_t)
-        entropy_np          = entropy_np/(1.0 + 10.0*self.ext_reward_running_stats.mean)
+        #entropy_np          = entropy_np/(1.0 + 10.0*self.ext_reward_running_stats.mean)
         entropy_np          = numpy.clip(entropy_np, -1.0, 1.0)
 
         states, rewards, dones, _ = self.envs.step(actions)
