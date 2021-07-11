0e3260991e1992ea1ae0583ebe61dd978ba7798d
michalnand <michal.nand@gmail.com>
2021-07-11 06:48:57 +0200

    ppo reachability

--- RLAgents/lib_agents/AgentPPOEntropy.py
+++ RLAgents/lib_agents/AgentPPOEntropy.py
@@ -92,7 +92,9 @@ class AgentPPOEntropy():
         
         for e in range(self.actors): 
             if dones[e]:
-                self.states[e] = self.envs.reset(e).copy()
+                s = self.envs.reset(e).copy()
+                self.states[e] = s.copy()
+                self.episodic_memory[e].reset(torch.from_numpy(s).to(self.model_ppo.device))
 
         #collect stats
         k = 0.02
