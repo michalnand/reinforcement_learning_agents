4c8ef220aa098572ccd4e12dc2335683ebc472c9
michalnand <michal.nand@gmail.com>
2021-08-28 10:32:34 +0200

    ppo rnd explore exploit

--- RLAgents/lib_agents/GoalsBuffer.py
+++ RLAgents/lib_agents/GoalsBuffer.py
@@ -87,10 +87,10 @@ class GoalsBuffer:
         reward_goal_steps  = numpy.tanh(0.1*faster)*reached_goals
 
         
-        '''
         if reached_goals[0]:
             print("goal reached", reward_reached_goals[0], reward_goal_steps[0], "\n\n")
 
+        '''
         idx = self.goal_idx[0]
         self._visualise(states_t[0], self.current_goal[0], self.desired_goals_b[0], self.reward_ext_b[idx], self.reward_int_b[idx])
         '''
@@ -109,8 +109,6 @@ class GoalsBuffer:
                 self.visited_count_b[self.total_goals]= 1
 
                 self.total_goals = self.total_goals + 1
-
-        #indices = self.indices.detach().to("cpu").numpy()
 
                
         #add higher reward
