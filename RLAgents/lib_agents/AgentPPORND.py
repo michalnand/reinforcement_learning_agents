ea3e422b82fa075b7d0fed3bc034b8a69415e2f5
michalnand <michal.nand@gmail.com>
2021-10-18 05:33:03 +0200

    rnd multi

--- RLAgents/lib_agents/AgentPPORND.py
+++ RLAgents/lib_agents/AgentPPORND.py
@@ -272,7 +272,7 @@ class AgentPPORND():
         mean = torch.from_numpy(self.states_running_stats.mean).to(state_t.device).float()
         std  = torch.from_numpy(self.states_running_stats.std).to(state_t.device).float()
         
-        state_norm_t = state_t - mean  
-        #state_norm_t = torch.clamp((state_t - mean)/std, -5.0, 5.0)
+        #state_norm_t = state_t - mean  
+        state_norm_t = torch.clamp((state_t - mean)/std, -5.0, 5.0)
 
         return state_norm_t 
