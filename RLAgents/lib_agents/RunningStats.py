c62b05fe789f33bd544fa4d3da591da6d69ade90
michalnand <michal.nand@gmail.com>
2021-11-01 16:35:34 +0100

    agent rnd multi

--- RLAgents/lib_agents/RunningStats.py
+++ RLAgents/lib_agents/RunningStats.py
@@ -1,22 +1,5 @@
 import numpy
 
-'''
-class RunningStats:
-    def __init__(self, shape = (), initial_value = None):
-        self.mean  = numpy.zeros(shape)
-        self.std   = 0.01*numpy.ones(shape) 
-
-        if initial_value is not None:
-            self.mean   = initial_value.mean(axis=0)
-            self.std    = initial_value.std(axis=0)
-    
-    def update(self, x, alpha = 0.001):    
-        mean        = x.mean(axis=0)
-        std         = x.std(axis=0)  
-
-        self.mean   = (1.0 - alpha)*self.mean   + alpha*mean
-        self.std    = (1.0 - alpha)*self.std    + alpha*std
-'''
 
 class RunningStats:
     def __init__(self, shape, initial_value=None):
