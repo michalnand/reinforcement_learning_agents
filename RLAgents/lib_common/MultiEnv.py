864208dd5504c88fd68d12d95ab3f9efc75e7b81
michalnand <michal.nand@gmail.com>
2021-11-29 17:01:12 +0100

    rnd goals

--- RLAgents/lib_common/MultiEnv.py
+++ RLAgents/lib_common/MultiEnv.py
@@ -240,7 +240,7 @@ def env_process_main(id, envs_count, child_conn, env_name, wrapper):
 
 
 class MultiEnvParallel:
-	def __init__(self, env_name, wrapper, envs_count, threads_count = 4):
+	def __init__(self, env_name, wrapper, envs_count, threads_count = 8):
 		try:
 			dummy_env 	= gym.make(env_name)
 			if wrapper is not None:
