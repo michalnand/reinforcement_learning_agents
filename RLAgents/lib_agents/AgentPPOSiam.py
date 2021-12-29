5264d98d51fe3d6120043ead858e9e0fbe54dee3
michalnand <michal.nand@gmail.com>
2021-12-29 16:39:33 +0100

    ppo sim siam

--- RLAgents/lib_agents/AgentPPOSiam.py
+++ RLAgents/lib_agents/AgentPPOSiam.py
@@ -284,7 +284,7 @@ class AgentPPOSiam():
     def _aug(self, x, k = 0.1):
         #x  = self._aug_random_flip(x, dim=1)
         #x  = self._aug_random_flip(x, dim=2)
-        x  = self._aug_resize(x, dim=2)
+        x  = self._aug_resize(x, scale = 4) 
         x  = self._aug_random_noise(x,  k)
   
         return x
