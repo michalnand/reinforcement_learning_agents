7596ec7305cc2b36835aaefaa273dce624bc8ac4
michalnand <michal.nand@gmail.com>
2022-01-02 17:11:59 +0100

    ppo rnd siam goals

--- RLAgents/lib_agents/AgentPPORNDSiam.py
+++ RLAgents/lib_agents/AgentPPORNDSiam.py
@@ -371,7 +371,8 @@ class AgentPPORNDSiam():
         true_negative = numpy.sum(1.0*(target < 0.5)*(predicted > confidence))
         acc = 100.0*(true_positive + true_negative)/target.shape[0]
 
-        print((za**2).mean(), (zb**2).mean())
+        norm = ((za**2).sim(dim=1))**0.5
+        print(loss, norm.mean())
 
         return loss, acc
     
