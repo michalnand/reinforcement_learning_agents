175482d395543ea56466fdd8fca5bceaf5b65880
michalnand <michal.nand@gmail.com>
2022-01-02 21:01:23 +0100

    ppo rnd siam goals

--- RLAgents/lib_agents/AgentPPORNDSiam.py
+++ RLAgents/lib_agents/AgentPPORNDSiam.py
@@ -411,8 +411,9 @@ class AgentPPORNDSiam():
                     self.envs.reset(e)
 
     def _aug(self, x):
-        #x  = self._aug_random_flip(x, p = 0.25, dim=1)
-        #x  = self._aug_random_flip(x, p = 0.25, dim=2)
+        x  = self._aug_random_flip(x, p = 0.25, dim=1)
+        x  = self._aug_random_flip(x, p = 0.25, dim=2)
+        
         x  = self._aug_resize(x, p = 0.5, scale = 2) 
         x  = self._aug_resize(x, p = 0.25, scale = 4) 
         x  = self._aug_random_noise(x, k = 0.2)
