b1df627969361a10cd65be176ea525dc73b98b6c
michalnand <michal.nand@gmail.com>
2022-01-05 12:34:35 +0100

    ppo rnd siam goals

--- RLAgents/lib_agents/AgentPPOContinuous.py
+++ RLAgents/lib_agents/AgentPPOContinuous.py
@@ -143,7 +143,7 @@ class AgentPPOContinuous():
         
         policy      = -torch.exp(log_probs_new - log_probs_old)*advantages
 
-        kl_div      = torch.exp(log_probs_old)*(log_probs_old - log_probs_new)
+        kl_div      = torch.exp(log_probs_new)*(log_probs_new - log_probs_old)
 
 
         loss_policy = policy + self.kl_beta*kl_div
