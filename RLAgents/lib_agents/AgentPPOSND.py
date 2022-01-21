e7c50a9e57abd98ae8b19aaac8e229e2f029cab3
michalnand <michal.nand@gmail.com>
2022-01-21 10:55:12 +0100

    ppo rnd siam goals

--- RLAgents/lib_agents/AgentPPOSND.py
+++ RLAgents/lib_agents/AgentPPOSND.py
@@ -452,7 +452,7 @@ class AgentPPOSND():
         #x = self._aug_random_apply(x, 0.1, self._aug_flip_vertical) 
         #x = self._aug_random_apply(x, 0.1, self._aug_flip_horizontal)
  
-        #x = self._aug_random_apply(x, 0.25, self._aug_mask)
+        x = self._aug_random_apply(x, 0.25, self._aug_mask)
         x = self._aug_random_apply(x, 0.5, self._aug_resize2)
         x = self._aug_random_apply(x, 0.25, self._aug_resize4)
         x = self._aug_noise(x, k = 0.2)
@@ -470,7 +470,7 @@ class AgentPPOSND():
     def _aug_flip_horizontal(self, x):
         return torch.flip(x, [2])
  
-    def _aug_mask(self, x, p = 0.05):
+    def _aug_mask(self, x, p = 0.1):
         mask = 1.0*(torch.rand_like(x) < (1.0 - p))
         return x*mask 
 
