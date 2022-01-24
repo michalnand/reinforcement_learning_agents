939945c577d5b70b0e19d3334267bacd277c94fa
michalnand <michal.nand@gmail.com>
2022-01-24 06:31:39 +0100

    snd agent

--- RLAgents/lib_agents/AgentPPOSND.py
+++ RLAgents/lib_agents/AgentPPOSND.py
@@ -446,7 +446,7 @@ class AgentPPOSND():
                     self.envs.reset(e)
 
     def _aug(self, x):
-        x = self._aug_random_apply(x, 0.25, self._aug_mask)
+        x = self._aug_random_apply(x, 0.5, self._aug_mask)
         x = self._aug_random_apply(x, 0.5, self._aug_resize2)
         x = self._aug_noise(x, k = 0.2)
         #x = self._aug_random_apply(x, 0.25, self._aug_noise)
