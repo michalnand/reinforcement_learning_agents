91c9cc2e66709f7a6da2e95c0eb80d362a458011
michalnand <michal.nand@gmail.com>
2022-02-13 08:06:40 +0100

    snd agent

--- RLAgents/lib_agents/PolicyBufferIM.py
+++ RLAgents/lib_agents/PolicyBufferIM.py
@@ -111,13 +111,13 @@ class PolicyBufferIM:
         return states, states_next, logits, actions, returns_ext, returns_int, advantages_ext, advantages_int 
     
     def sample_states(self, batch_size, device):
-        indices         = numpy.random.randint(0, self.envs_count*self.buffer_size, size=batch_size*self.envs_count)
+        indices         = numpy.random.randint(0, self.envs_count*self.buffer_size, size=batch_size)
         indices_next    = numpy.clip(indices + 1, 0, self.envs_count*self.buffer_size-1)
 
         states          = torch.from_numpy(numpy.take(self.states, indices, axis=0)).to(device).float()/self.scale
         states_next     = torch.from_numpy(numpy.take(self.states, indices_next, axis=0)).to(device).float()/self.scale
       
-        return states, states_next
+        return states, states_next 
    
 
     '''
