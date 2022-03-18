d6c1c739601addb7829b83ed33c48735b6f20fd1
michalnand <michal.nand@gmail.com>
2022-03-18 19:43:30 +0100

    ppo snd 3e

--- RLAgents/lib_agents/FeaturesBuffer.py
+++ RLAgents/lib_agents/FeaturesBuffer.py
@@ -35,7 +35,7 @@ class FeaturesBuffer:
 
         return var
     '''
-
+ 
     def compute(self, values_t, top_n = 32):
         #difference
         dif = self.buffer - values_t
@@ -49,10 +49,6 @@ class FeaturesBuffer:
         if top_n is not None:
             distances = torch.sort(distances, dim=0)[0]
             distances = distances[0:top_n,:]
-
-        print(distances)
-        print(distances.shape)
-        print("\n\n\n")
  
         mean = distances.mean(dim=0)
        
