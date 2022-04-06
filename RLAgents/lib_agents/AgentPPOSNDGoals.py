49b673aa806fe7eabac577b4c6f8281ba1dd90ac
michalnand <michal.nand@gmail.com>
2022-04-06 17:46:43 +0200

    ppo snd goals

--- RLAgents/lib_agents/AgentPPOSNDGoals.py
+++ RLAgents/lib_agents/AgentPPOSNDGoals.py
@@ -258,8 +258,10 @@ class AgentPPOSNDGoals():
  
 
         #random ids for goal based
-        goal_based_ids = numpy.random.randint(0, self.envs_count, int(100*self.goal_policy_ratio))
+        goal_based_ids = numpy.random.choice(self.envs_count, int(100*self.goal_policy_ratio), replace=False)
         goal_based_ids = numpy.sort(goal_based_ids)
+
+        print(">>> ", goal_based_ids)
 
         for step in range(buffer_size):
             states      = self.states_buffer.states[step]
