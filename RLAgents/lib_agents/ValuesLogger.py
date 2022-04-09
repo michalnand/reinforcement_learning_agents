0f0e5e3348c727a59b6c011eb348476a172e4ea3
michalnand <michal.nand@gmail.com>
2022-04-09 08:41:09 +0200

    ppo snd goals

--- RLAgents/lib_agents/ValuesLogger.py
+++ RLAgents/lib_agents/ValuesLogger.py
@@ -5,7 +5,7 @@ class ValuesLogger:
         self.values = {}
 
     def add(self, name, value, smoothing = 0.02):
-        if name in self.values
+        if name in self.values:
             self.values[name] = (1.0 - smoothing)*self.values[name] + smoothing*value
         else: 
             self.values[name] = value
