f0725bc5be659129ffe403980fd9804af5c74201
michalnand <michal.nand@gmail.com>
2022-04-09 08:41:55 +0200

    ppo snd goals

--- RLAgents/lib_common/WrapperMontezuma.py
+++ RLAgents/lib_common/WrapperMontezuma.py
@@ -178,7 +178,7 @@ class LifeLostEnv(gym.Wrapper):
 
         lives = self.env.unwrapped.ale.lives()
         if lives < self.lives:
-            reward+= -0.0000000001
+            reward+= -1.0
         
         self.lives = lives
         return obs, reward, done, info
@@ -236,7 +236,6 @@ def WrapperMontezuma(env, height = 96, width = 96, frame_stacking = 4, max_steps
     env = RepeatActionEnv(env) 
     env = ResizeEnv(env, height, width, frame_stacking)
     env = VisitedRoomsEnv(env)
-    env = LifeLostEnv(env)
     env = RawScoreEnv(env, max_steps)
 
     return env
