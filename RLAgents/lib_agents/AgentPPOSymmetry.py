426095d4e0facd8a465aa70840f848a901ee6d23
michalnand <michal.nand@gmail.com>
2022-05-04 20:47:36 +0200

    snd agent

--- RLAgents/lib_agents/AgentPPOSymmetry.py
+++ RLAgents/lib_agents/AgentPPOSymmetry.py
@@ -256,9 +256,9 @@ class AgentPPOSymmetry():
 
         #similar features for transitions caused by same action
         #conservation of rules - the rules are the same, no matters the state
-        #w           = 1.0 - 1.0/self.actions_count
+        w           = 1.0 - 1.0/self.actions_count
         #loss_bce    = -( w*labels*torch.log(probs) + (1.0 - w)*(1.0 - labels)*torch.log(1.0 - probs) )
-        loss_bce    = -(labels*torch.log(probs) + (1.0 - labels)*torch.log(1.0 - probs))
+        loss_bce    = (labels - logits)**2 
         loss_bce    = loss_bce.mean()  
 
         #entropy regularisation, maxmise entropy
