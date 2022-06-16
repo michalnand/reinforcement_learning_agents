091edd29324a24ade6eb493390d97efaaa02d74c
michalnand <michal.nand@gmail.com>
2022-06-16 21:10:08 +0200

    ppo cnd

--- RLAgents/lib_agents/AgentPPOCND.py
+++ RLAgents/lib_agents/AgentPPOCND.py
@@ -459,6 +459,8 @@ class AgentPPOCND():
 
         loss = loss_mse + loss_bce + loss_magnitude
 
+        print("loss = ", loss_mse, loss_bce)
+
         self.values_logger.add("cnd_magnitude", magnitude.detach().to("cpu").numpy())
     
         return loss
