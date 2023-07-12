f932ff562ea5b64bc84354ec31f06178dba72fab
michalnand <michal.nand@gmail.com>
2023-07-12 22:08:50 +0200

    ppo_snd_ca

--- RLAgents/lib_agents/AgentPPOSNDCA.py
+++ RLAgents/lib_agents/AgentPPOSNDCA.py
@@ -259,6 +259,7 @@ class AgentPPOSNDCA():
 
         small_batch = 16*self.batch_size 
 
+        accuracy_all    = 0
         for e in range(self.training_epochs):
             for batch_idx in range(batch_count):
                 states, logits, actions, returns_ext, returns_int, advantages_ext, advantages_int, hidden_state = self.policy_buffer.sample_batch(self.batch_size, self.device)
@@ -285,7 +286,7 @@ class AgentPPOSNDCA():
 
 
                 loss_target_all = 0
-                accuracy_all    = 0
+
                 for i in range(len(self.model_snd_targets)):
                     #sample smaller batch for self supervised loss, different distances for different models
                     states_now, states_next, states_similar, states_random, actions, relations = self.policy_buffer.sample_states_action_pairs(small_batch, self.device, self.similar_states_distances[i])
@@ -322,7 +323,9 @@ class AgentPPOSNDCA():
                 self.values_logger.add("loss_target",   loss_target_all)
                 self.values_logger.add("loss_distillation", loss_distillation.detach().to("cpu").numpy())
 
-                self.info_logger["accuracy"] = list(numpy.round(accuracy_all, 3))
+        accuracy_all = accuracy_all/(self.training_epochs*batch_count)
+
+        self.info_logger["accuracy"] = list(numpy.round(accuracy_all, 3))
                 
         self.policy_buffer.clear() 
 
