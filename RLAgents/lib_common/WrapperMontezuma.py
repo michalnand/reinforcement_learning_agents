80e4df6c9b92d401c42e54ad36f23fc23fc65209
michalnand <michal.nand@gmail.com>
2023-09-07 09:40:26 +0200

    ppo_ce

--- RLAgents/lib_common/WrapperMontezuma.py
+++ RLAgents/lib_common/WrapperMontezuma.py
@@ -299,9 +299,9 @@ def WrapperMontezuma(env, height = 96, width = 96, frame_stacking = 4, max_steps
     env = RepeatActionEnv(env) 
     #env = ColectStatesEnv(env)
     env = ResizeEnv(env, height, width, frame_stacking)
-    
+     
     env = VisitedRoomsEnv(env)    
-    env = RawScoreEnv(env, max_steps, adaptive_scale=False) 
+    env = RawScoreEnv(env, max_steps) 
 
     return env
 
