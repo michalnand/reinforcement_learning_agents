2dfb156066169fa69b1c60de06eb2afe39d19d0a
Michal Chovanec <michal@Michals-MacBook-Air.local>
2023-10-26 15:45:39 +0200

    flow explorer

--- RLAgents/lib_agents/AgentPPOFE.py
+++ RLAgents/lib_agents/AgentPPOFE.py
@@ -281,7 +281,7 @@ class AgentPPOFE():
                 error  = self.reward_int_req - rewards_int.mean()
                 self.im_loss_coeff+= -self.dim_loss_coeff*error
 
-                self.im_loss_coeff = torch.clip(self.im_loss_coeff, 0.001, 10.0)
+                self.im_loss_coeff = torch.clip(self.im_loss_coeff, 0.01, 1.0)
 
                 #IM model loss
                 loss_im = loss_im_self_supervised + self.im_loss_coeff*loss_im_distillation
