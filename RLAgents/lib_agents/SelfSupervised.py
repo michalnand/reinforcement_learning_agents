43ecea29d7127315ead209e8c712a7198140412d
Michal Chovanec <michal@Michals-MacBook-Air.local>
2023-11-05 17:17:27 +0100

    mast vicreg loss

--- RLAgents/lib_agents/SelfSupervised.py
+++ RLAgents/lib_agents/SelfSupervised.py
@@ -51,13 +51,11 @@ def loss_vicreg_mast(model_forward_func, augmentations, states_now, states_simil
     #used augmentations mask
     #mask_aug.shape = (augs_count, batch_size, 1)
     mask_aug = torch.clip(mask_a_aug + mask_b_aug, 0.0, 1.0)
-
-   
     mask_aug = mask_aug.unsqueeze(2)
     
     # obtain features from model
     #za.shape   = (batch_size, features_count)
-    #mask.shape = (augs_count, 1, features_count)
+    #mask_w.shape = (augs_count, 1, features_count)
     za, mask_w  = model_forward_func(xa_aug)  
     zb,    _    = model_forward_func(xb_aug) 
 
