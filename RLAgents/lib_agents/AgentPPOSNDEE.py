b215d9a8b37fe0358f4ddaf294adaf8a624b8760
Michal Chovanec <michal@192.168.0.106>
2023-11-26 18:59:54 +0100

    snd explore exploit

--- RLAgents/lib_agents/AgentPPOSNDEE.py
+++ RLAgents/lib_agents/AgentPPOSNDEE.py
@@ -413,12 +413,7 @@ class AgentPPOSNDEE():
     
 
     def _compose_state(self, states, agent_mode):
-
-        print("\n\n")
-        print(self.state_shape, states.shape)
-        print("\n\n")
-
-        result = torch.zeros(self.state_shape, dtype=torch.float32, device=self.device)
+        result = torch.zeros((self.envs_count, ) + self.state_shape, dtype=torch.float32, device=self.device)
 
         result[:, 0:states.shape[0]] = torch.from_numpy(states).to(self.device)
         result[:, -1] = agent_mode 
