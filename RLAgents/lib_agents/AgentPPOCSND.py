f83479007774efd9c276dd1df585fe8f8ad8fa05
Michal Chovanec <michal@192.168.0.102>
2023-12-01 16:39:29 +0100

    snd explore exploit

--- RLAgents/lib_agents/AgentPPOCSND.py
+++ RLAgents/lib_agents/AgentPPOCSND.py
@@ -388,19 +388,20 @@ class AgentPPOCSND():
 
         return loss 
     
-    '''
+    
     def _causality_loss(self, forward_func, states_a, states_b, distances):
         
         distances_target = torch.sgn(distances)*torch.log(1.0 + torch.abs(distances))
+        distances_target = distances_target.detach()
 
         distances_pred   = forward_func(states_a, states_b)
 
         loss = ((distances_target.detach() - distances_pred)**2).mean()
 
         return loss
+    
+
     '''
-
-
     def _causality_loss(self, forward_func, states_a, states_b, distances):
         
         labels = (distances > 0.0).float()
@@ -418,7 +419,7 @@ class AgentPPOCSND():
         self.values_logger.add("accuracy", acc.detach().cpu().numpy())
 
         return loss
-
+    '''
         
    
     #compute internal motivation
