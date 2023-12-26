293b659eaaf9581109a55b1c5efbd859463efe52
michalnand <michal.nand@gmail.com>
2023-12-26 20:45:21 +0100

    ppo competitive

--- RLAgents/lib_common/TrainingCompetitive.py
+++ RLAgents/lib_common/TrainingCompetitive.py
@@ -117,7 +117,8 @@ class TrainingCompetitive:
             rewards_sum+= rewards
             score_sum+= score
 
-            print(score_sum[0:10])
+            if score.sum() > 0:
+                print(score[0:10], score_sum[0:10])
 
 
             time_remaining = self._estimate_time(iteration)
