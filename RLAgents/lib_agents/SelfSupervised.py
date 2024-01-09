7757cb1f8a95a332e84bf7c635df445ef246e38b
michalnand <michal.nand@gmail.com>
2024-01-09 20:54:07 +0100

    jepa loss

--- RLAgents/lib_agents/SelfSupervised.py
+++ RLAgents/lib_agents/SelfSupervised.py
@@ -33,6 +33,67 @@ def loss_vicreg_direct(za, zb):
     return loss
 
 
+'''
+1, input images : xa, xb
+
+2, first the features are computed :
+        za = f(xa)
+        zb = f(xb)
+
+3,  for za, zb are variance (std_loss) and covariance (cov_loss) terms, regularizing z-space
+
+4,  hidden information is computed :
+        ha = h(za)
+        hb = h(zb)
+    which contains transformerd information about za, zb
+    in final, goal is to minimize this ha, hb, to provide minimum information
+
+5, predictor is computed as : 
+        pa = p(za, hb)
+        pb = p(zb, ha)
+
+        and invariance (mse loss) is called :
+        MSE(za - pb), MSE(zb - pa)
+
+        essence of JEPA is : p-model can easily predict za, 
+        because pa contains not only augmented context from za,
+        but also "cheating" information about zb (called hidden information)
+        
+        however, h-features are forced to minimize information, which leads to good
+        za, zb features, and h contains only the necessary information 
+        from za which can't be extracted from zb (and vice versa)
+'''
+def loss_jepa_direct(za, zb, pa, pb, ha, hb):
+    eps = 0.0001 
+ 
+    # invariance loss
+    sim_loss = ((za - pb)**2).mean() + ((zb - pa)**2).mean()
+
+    # variance loss
+    std_za = torch.sqrt(za.var(dim=0) + eps)
+    std_zb = torch.sqrt(zb.var(dim=0) + eps) 
+    
+    std_loss = torch.mean(torch.relu(1.0 - std_za)) 
+    std_loss+= torch.mean(torch.relu(1.0 - std_zb))
+   
+    # covariance loss 
+    za_norm = za - za.mean(dim=0)
+    zb_norm = zb - zb.mean(dim=0)
+    cov_za = (za_norm.T @ za_norm) / (za.shape[0] - 1.0)
+    cov_zb = (zb_norm.T @ zb_norm) / (zb.shape[0] - 1.0)
+    
+    cov_loss = _off_diagonal(cov_za).pow_(2).sum()/za.shape[1] 
+    cov_loss+= _off_diagonal(cov_zb).pow_(2).sum()/zb.shape[1]
+
+    #hidden information loss, minimize
+    hidden_loss = torch.abs(ha).mean() + torch.abs(hb).mean() 
+
+    # total vicreg loss
+    loss = 1.0*sim_loss + 1.0*std_loss + (1.0/25.0)*cov_loss + 10.0*hidden_loss
+
+    return loss
+
+
 
 def loss_vicreg_contrastive_direct(za, zb, steps_a, steps_b):
     eps = 0.0001 
@@ -90,6 +151,16 @@ def loss_vicreg(model_forward_func, augmentations, states_a, states_b):
     zb = model_forward_func(xb_aug) 
 
     return loss_vicreg_direct(za, zb)
+
+
+def loss_jepa(model_forward_func, augmentations, states_a, states_b):
+    xa_aug, _ = augmentations(states_a)
+    xb_aug, _ = augmentations(states_b)
+
+    za, zb, pa, pb, ha, hb = model_forward_func(xa_aug, xb_aug)  
+
+    return loss_jepa_direct(za, zb, pa, pb, ha, hb)
+
 
 def loss_vicreg_contrastive(model_forward_func, augmentations, states_a, states_b, episode_steps_a, episode_steps_b):
     xa_aug, _ = augmentations(states_a)
