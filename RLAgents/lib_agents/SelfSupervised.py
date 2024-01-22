dac41267e25aa5cfba9d9c21d57891189cf25e70
michalnand <michal.nand@gmail.com>
2024-01-22 10:18:46 +0100

    jepa ppo

--- RLAgents/lib_agents/SelfSupervised.py
+++ RLAgents/lib_agents/SelfSupervised.py
@@ -112,6 +112,50 @@ def loss_vicreg_jepa_direct(za, zb, pa, pb, ha, hb, hidden_coeff = 0.01):
     info = [z_mag, z_mag_std, h_mag, h_mag_std]
 
     return loss, info
+
+
+
+
+def loss_vicreg_jepa_proj_direct(za, zb, pa, pb, ha, hb, proj_za, proj_zb, hidden_coeff = 0.01):
+    eps = 0.0001 
+ 
+    # invariance loss
+    sim_loss = ((za - pb)**2).mean() + ((zb - pa)**2).mean()
+
+    # variance loss
+    std_za = torch.sqrt(proj_za.var(dim=0) + eps)
+    std_zb = torch.sqrt(proj_zb.var(dim=0) + eps) 
+    
+    std_loss = torch.mean(torch.relu(1.0 - std_za)) 
+    std_loss+= torch.mean(torch.relu(1.0 - std_zb))
+   
+    # covariance loss 
+    za_norm = proj_za - proj_za.mean(dim=0)
+    zb_norm = proj_zb - proj_zb.mean(dim=0)
+    cov_za = (za_norm.T @ za_norm) / (proj_za.shape[0] - 1.0)
+    cov_zb = (zb_norm.T @ zb_norm) / (proj_zb.shape[0] - 1.0)
+     
+    cov_loss = _off_diagonal(cov_za).pow_(2).sum()/proj_za.shape[1] 
+    cov_loss+= _off_diagonal(cov_zb).pow_(2).sum()/proj_zb.shape[1]
+
+    #hidden information loss, enforce sparsity, and minimize batch-wise variance
+    h_mag = torch.abs(ha).mean() + torch.abs(hb).mean() 
+    h_std = (ha.std(dim=0)).mean() + (hb.std(dim=0)).mean()
+    hidden_loss = h_mag + h_std
+
+
+    # total loss, vicreg + info-min
+    loss = 0.5*sim_loss + 1.0*std_loss + (1.0/25.0)*cov_loss + hidden_coeff*hidden_loss
+
+    #info for log
+    z_mag     = round(((za**2).mean()).detach().cpu().numpy().item(), 6)
+    z_mag_std = round(((za**2).std()).detach().cpu().numpy().item(), 6)
+    h_mag     = round(((ha**2).mean()).detach().cpu().numpy().item(), 6)
+    h_mag_std = round(((ha**2).std()).detach().cpu().numpy().item(), 6)
+
+    info = [z_mag, z_mag_std, h_mag, h_mag_std]
+
+    return loss, info
  
 
 
@@ -119,8 +163,8 @@ def loss_vicreg(model_forward_func, augmentations, states_a, states_b):
     xa_aug, _ = augmentations(states_a)
     xb_aug, _ = augmentations(states_b)
 
-    za = model_forward_func(xa_aug)  
-    zb = model_forward_func(xb_aug) 
+    za = model_forward_func(xa_aug)
+    zb = model_forward_func(xb_aug)
 
     return loss_vicreg_direct(za, zb)
 
@@ -129,77 +173,13 @@ def loss_vicreg_jepa(model_forward_func, augmentations, states_a, states_b, hidd
     xa_aug, _ = augmentations(states_a)
     xb_aug, _ = augmentations(states_b)
 
-    za, zb, pa, pb, ha, hb = model_forward_func(xa_aug, xb_aug)  
+    res = model_forward_func(xa_aug, xb_aug)  
 
-    return loss_vicreg_jepa_direct(za, zb, pa, pb, ha, hb, hidden_coeff)
+    if len(res) == 6:
+        za, zb, pa, pb, ha, hb = res
+        loss = loss_vicreg_jepa_direct(za, zb, pa, pb, ha, hb, hidden_coeff)
+    elif len(res) == 8:
+        za, zb, pa, pb, ha, hb, proj_za, proj_zb = res
+        loss = loss_vicreg_jepa_proj_direct(za, zb, pa, pb, ha, hb, proj_za, proj_zb, hidden_coeff)
 
-
-def loss_vicreg_temporal(model_forward_func, augmentations, states_a, states_b, hidden_a, hidden_b, max_seq_length):
-    xa_aug, _ = augmentations(states_a)
-    xb_aug, _ = augmentations(states_b)  
-
-    za, _ = model_forward_func(xa_aug, hidden_a, max_seq_length)  
-    zb, _ = model_forward_func(xb_aug, hidden_b, max_seq_length) 
-
-    '''
-    #reshape for vicreg loss (batch, seq*features)
-    za  = za.reshape((za.shape[0], za.shape[1]*za.shape[2]))
-    zb  = zb.reshape((zb.shape[0], zb.shape[1]*zb.shape[2]))
-    '''
-
-    #reshape for vicreg loss (batch*seq, features)
-    za  = za.reshape((za.shape[0]*za.shape[1], za.shape[2]))
-    zb  = zb.reshape((zb.shape[0]*zb.shape[1], zb.shape[2]))
-
-    '''
-    #select only last item from seq
-    za  = za[:, -1, :]
-    zb  = zb[:, -1, :]
-    '''
-
-    return loss_vicreg_direct(za, zb)
-
- 
-
-def loss_vicreg_jepa_temporal(model_forward_func, augmentations, states_a, states_b, hidden_a, hidden_b, max_seq_length):
-    xa_aug, _ = augmentations(states_a)
-    xb_aug, _ = augmentations(states_b)  
-
-    za, zb, pa, pb, ha, hb = model_forward_func(xa_aug, xb_aug, hidden_a, hidden_b, max_seq_length)  
-
-    '''
-    #reshape for vicreg loss (batch, seq*features)
-    za  = za.reshape((za.shape[0], za.shape[1]*za.shape[2]))
-    zb  = zb.reshape((zb.shape[0], zb.shape[1]*zb.shape[2]))
-
-    pa  = pa.reshape((pa.shape[0], pa.shape[1]*pa.shape[2]))
-    pb  = pb.reshape((pb.shape[0], pb.shape[1]*pb.shape[2]))
-
-    ha  = ha.reshape((ha.shape[0], ha.shape[1]*ha.shape[2]))
-    hb  = hb.reshape((hb.shape[0], hb.shape[1]*hb.shape[2]))
-    ''' 
-    
-    '''
-    #select only last item from seq
-    za  = za[:, -1, :] 
-    zb  = zb[:, -1, :]
-
-    pa  = pa[:, -1, :]
-    pb  = pb[:, -1, :]
-
-    ha  = ha[:, -1, :]
-    hb  = hb[:, -1, :]
-    '''
-
-    #reshape for vicreg loss (batch*seq, features)
-    za  = za.reshape((za.shape[0]*za.shape[1], za.shape[2]))
-    zb  = zb.reshape((zb.shape[0]*zb.shape[1], zb.shape[2]))
-
-    pa  = pa.reshape((pa.shape[0]*pa.shape[1], pa.shape[2]))
-    pb  = pb.reshape((pb.shape[0]*pb.shape[1], pb.shape[2]))
-
-    ha  = ha.reshape((ha.shape[0]*ha.shape[1], ha.shape[2]))
-    hb  = hb.reshape((hb.shape[0]*hb.shape[1], hb.shape[2]))
-
-    return loss_vicreg_jepa_direct(za, zb, pa, pb, ha, hb)
-
+    return loss
