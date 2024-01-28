7183e8dbe93debf5b8108051a56a8d1018f6420f
michalnand <michal.nand@gmail.com>
2024-01-28 13:43:21 +0100

    ppo jepa

--- RLAgents/lib_agents/AgentPPOJEPA_orig.py
+++ RLAgents/lib_agents/AgentPPOJEPA_orig.py
@@ -10,7 +10,7 @@ from .Augmentations         import *
   
 
 class AgentPPOJEPA():   
-    def __init__(self, envs, Model, config):
+    def __init__(self, envs, ModelPPO, ModelIM, config):
 
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -36,10 +36,17 @@ class AgentPPOJEPA():
         self.envs_count         = config.envs_count
 
 
-        if config.self_supervised_loss == "vicreg_jepa":
-            self._self_supervised_loss = loss_vicreg_jepa
+                
+        if config.ppo_self_supervised_loss == "vicreg":
+            self._ppo_self_supervised_loss = loss_vicreg
         else:
-            self._self_supervised_loss = None 
+            self._ppo_self_supervised_loss = None
+
+       
+        if config.target_self_supervised_loss == "vicreg_jepa":
+            self._target_self_supervised_loss = loss_vicreg_jepa
+        else:
+            self._target_self_supervised_loss = None
 
         self.similar_states_distance = config.similar_states_distance
         self.state_normalise      = config.state_normalise
@@ -48,7 +55,8 @@ class AgentPPOJEPA():
         self.augmentations                  = config.augmentations
         self.augmentations_probs            = config.augmentations_probs
         
-        print("self_supervised_loss                  = ", self._self_supervised_loss)
+        print("ppo_self_supervised_loss              = ", self._ppo_self_supervised_loss)
+        print("target_self_supervised_loss           = ", self._target_self_supervised_loss)
         print("augmentations                         = ", self.augmentations)
         print("augmentations_probs                   = ", self.augmentations_probs)
         print("reward_int_coeff                      = ", self.reward_int_coeff)
@@ -61,15 +69,21 @@ class AgentPPOJEPA():
         self.state_shape    = self.envs.observation_space.shape
         self.actions_count  = self.envs.action_space.n
 
-        #create model
-        self.model      = Model.Model(self.state_shape, self.actions_count)
-        self.model.to(self.device)
-        self.optimizer  = torch.optim.Adam(self.model.parameters(), lr=config.learning_rate)
+        #main ppo agent
+        self.model_ppo      = ModelPPO.Model(self.state_shape, self.actions_count)
+        self.model_ppo.to(self.device)
+        self.optimizer_ppo  = torch.optim.Adam(self.model_ppo.parameters(), lr=config.learning_rate_ppo)
 
-      
+        
+        #IM model
+        self.model_im      = ModelIM.Model(self.state_shape)
+        self.model_im.to(self.device)
+        self.optimizer_im  = torch.optim.Adam(self.model_im.parameters(), lr=config.learning_rate_im)
+    
         self.policy_buffer = PolicyBufferIMNew(self.steps, self.state_shape, self.actions_count, self.envs_count)
 
-        print(self.model)
+        print(self.model_ppo)
+        print(self.model_im)
      
         #optional, for state mean and variance normalisation        
         self.state_mean  = numpy.zeros(self.state_shape, dtype=numpy.float32)
@@ -83,8 +97,9 @@ class AgentPPOJEPA():
 
         #optional int reward normalisation
         self.reward_mean = 0.0
-        self.reward_var  = 1.0 
+        self.reward_var  = 1.0
 
+        self.states_t = torch.zeros((self.envs_count, ) + self.state_shape, dtype=torch.float32, device=self.device)
 
       
         self.iterations = 0 
@@ -96,8 +111,10 @@ class AgentPPOJEPA():
 
         self.values_logger.add("loss_ppo_actor",  0.0)
         self.values_logger.add("loss_ppo_critic", 0.0)
-        self.values_logger.add("loss_self_supervised", 0.0)
- 
+        self.values_logger.add("loss_ppo_self_supervised", 0.0)
+
+        self.values_logger.add("loss_im", 0.0)        
+
         self.info_logger = {} 
 
    
@@ -116,10 +133,11 @@ class AgentPPOJEPA():
         states_norm = self._state_normalise(states, training_enabled)
         
         #state to tensor
-        states_t = torch.tensor(states_norm, dtype=torch.float).to(self.device)
+        states_prev_t = self.states_t.clone()
+        self.states_t = torch.tensor(states_norm, dtype=torch.float).to(self.device)
 
         #compute model output
-        logits_t, values_ext_t, values_int_t = self.model.forward(states_t)
+        logits_t, values_ext_t, values_int_t = self.model_ppo.forward(self.states_t)
         
         #collect actions 
         actions = self._sample_actions(logits_t, legal_actions_mask)
@@ -128,8 +146,7 @@ class AgentPPOJEPA():
         states_new, rewards_ext, dones, _, infos = self.envs.step(actions)
 
         #internal motivation
-        rewards_int = self._internal_motivation(states_t)
-
+        rewards_int = self._internal_motivation(self.states_t)
         if self.int_reward_normalise:
             rewards_int = self.reward_int_coeff*self._reward_normalise(rewards_int)
         else:
@@ -139,7 +156,7 @@ class AgentPPOJEPA():
         
         #put into policy buffer
         if training_enabled:
-            states_t        = states_t.detach().to("cpu")
+            states_t        = self.states_t.detach().to("cpu")
             logits_t        = logits_t.detach().to("cpu")
             values_ext_t    = values_ext_t.squeeze(1).detach().to("cpu") 
             values_int_t    = values_int_t.squeeze(1).detach().to("cpu")
@@ -153,6 +170,10 @@ class AgentPPOJEPA():
             if self.policy_buffer.is_full():
                 self.train()
 
+                #print("mean, var = ", self.reward_mean, self.reward_var)
+
+                
+
         #collect stats
         self.values_logger.add("internal_motivation_mean", rewards_int.mean().detach().to("cpu").numpy())
         self.values_logger.add("internal_motivation_std" , rewards_int.std().detach().to("cpu").numpy())
@@ -165,7 +186,8 @@ class AgentPPOJEPA():
    
     
     def save(self, save_path):
-        torch.save(self.model.state_dict(), save_path + "trained/model.pt")
+        torch.save(self.model_ppo.state_dict(), save_path + "trained/model_ppo.pt")
+        torch.save(self.model_im.state_dict(), save_path + "trained/model_im.pt")
     
         if self.state_normalise:
             with open(save_path + "trained/" + "state_mean_var.npy", "wb") as f:
@@ -173,7 +195,8 @@ class AgentPPOJEPA():
                 numpy.save(f, self.state_var)
         
     def load(self, load_path):
-        self.model.load_state_dict(torch.load(load_path + "trained/model.pt", map_location = self.device))
+        self.model_ppo.load_state_dict(torch.load(load_path + "trained/model_ppo.pt", map_location = self.device))
+        self.model_im.load_state_dict(torch.load(load_path + "trained/model_im.pt", map_location = self.device))
         
         if self.state_normalise:
             with open(load_path + "trained/" + "state_mean_var.npy", "rb") as f:
@@ -210,28 +233,49 @@ class AgentPPOJEPA():
             for batch_idx in range(batch_count):
                 states, logits, actions, returns_ext, returns_int, advantages_ext, advantages_int, _ = self.policy_buffer.sample_batch(self.batch_size, self.device)
                 
-                # PPO model loss
-                loss_ppo = self._loss_ppo(states, logits, actions, returns_ext, returns_int, advantages_ext, advantages_int)
+                #train PPO model
+                loss_ppo     = self._loss_ppo(states, logits, actions, returns_ext, returns_int, advantages_ext, advantages_int)
                 
-                # sample batch pair for self supervised loss
-                states_now, states_similar = self.policy_buffer.sample_states_pairs(self.ss_batch_size, self.similar_states_distance, self.device)
+                #train ppo features, self supervised
+                if self._ppo_self_supervised_loss is not None:
+                    #sample smaller batch for self supervised loss
+                    states_now, states_similar = self.policy_buffer.sample_states_pairs(self.ss_batch_size, 0, self.device)
 
-                #train features, self supervised
-                loss_self_supervised, im_ssl = self._self_supervised_loss(self.model.forward_self_supervised, self._augmentations, states_now, states_similar, self.hidden_coeff)                
+                    loss_ppo_self_supervised, ppo_ssl = self._ppo_self_supervised_loss(self.model_ppo.forward_features, self._augmentations, states_now, states_similar)  
+                    self.info_logger["ppo_ssl"] = ppo_ssl
+                else:
+                    loss_ppo_self_supervised    = torch.zeros((1, ), device=self.device)[0]
 
-                #total loss
-                loss = loss_ppo + loss_self_supervised
+                #total PPO loss
+                loss = loss_ppo + loss_ppo_self_supervised
 
                 self.optimizer_ppo.zero_grad()        
                 loss.backward()
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), max_norm=0.5)
+                torch.nn.utils.clip_grad_norm_(self.model_ppo.parameters(), max_norm=0.5)
                 self.optimizer_ppo.step()
 
-                self.info_logger["im_ssl"] = im_ssl
-
-                self.values_logger.add("loss_self_supervised", loss_self_supervised.detach().cpu().numpy())
+                self.values_logger.add("loss_ppo_self_supervised", loss_ppo_self_supervised.detach().cpu().numpy())
         
-    
+        
+        #IM model training
+        batch_count = (samples_count//self.ss_batch_size)//2
+
+        #print("ssl_samples = ", batch_count*self.ss_batch_size)
+
+        for batch_idx in range(batch_count):
+            #sample smaller batch for self supervised loss
+            states_now, states_similar = self.policy_buffer.sample_states_pairs(self.ss_batch_size, self.similar_states_distance, self.device)
+            loss_im, im_ssl  = self._target_self_supervised_loss(self.model_im.forward_self_supervised, self._augmentations, states_now, states_similar, self.hidden_coeff)                
+
+            self.info_logger["im_ssl"] = im_ssl
+            
+            self.optimizer_im.zero_grad() 
+            loss_im.backward()
+            self.optimizer_im.step()
+
+            #log results
+            self.values_logger.add("loss_im", loss_im.detach().cpu().numpy())
+            
         self.policy_buffer.clear() 
 
         
@@ -239,7 +283,7 @@ class AgentPPOJEPA():
 
     def _loss_ppo(self, states, logits, actions, returns_ext, returns_int, advantages_ext, advantages_int):
 
-        logits_new, values_ext_new, values_int_new  = self.model.forward(states)
+        logits_new, values_ext_new, values_int_new  = self.model_ppo.forward(states)
 
         #critic loss
         loss_critic = ppo_compute_critic_loss(values_ext_new, returns_ext, values_int_new, returns_int)
@@ -253,6 +297,7 @@ class AgentPPOJEPA():
 
         #PPO main actor loss
         loss_policy, loss_entropy = ppo_compute_actor_loss(logits, logits_new, advantages_norm, actions, self.eps_clip, self.entropy_beta)
+
         loss_actor = loss_policy + loss_entropy
 
         #total loss
@@ -267,11 +312,9 @@ class AgentPPOJEPA():
 
     #compute internal motivations
     def _internal_motivation(self, states):         
-        za, zb, pa, pb, ha, hb = self.model.forward_self_supervised(states, states)
+        za, zb, pa, pb, ha, hb = self.model_im(states, states)
 
-        im_mse = ((za - pb)**2).mean(dim=-1) + ((zb - pa)**2).mean(dim=-1)
-        im_mse = 0.5*im_mse
-
+        im_mse      = 0.5 *( ((za - pb)**2).mean(dim=-1) + ((zb - pa)**2).mean(dim=-1) )
         return im_mse.detach().cpu()
  
 
