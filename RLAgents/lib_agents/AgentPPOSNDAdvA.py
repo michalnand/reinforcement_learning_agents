4ab0f317edd4f1854d88db5a8e502a58ad2b9184
michalnand <michal.nand@gmail.com>
2024-06-20 17:39:43 +0200

    hypercube vicreg loss

--- RLAgents/lib_agents/AgentPPOSNDAdvA.py
+++ RLAgents/lib_agents/AgentPPOSNDAdvA.py
@@ -45,11 +45,15 @@ class AgentPPOSNDAdvA():
        
         if config.rl_self_supervised_loss == "vicreg":
             self._rl_self_supervised_loss = loss_vicreg
+        elif config.im_self_supervised_loss == "vicreg_hypercube":
+            self._im_self_supervised_loss = loss_vicreg_hypercube
         else:
             self._rl_self_supervised_loss = None
 
         if config.im_self_supervised_loss == "vicreg":
             self._im_self_supervised_loss = loss_vicreg
+        elif config.im_self_supervised_loss == "vicreg_hypercube":
+            self._im_self_supervised_loss = loss_vicreg_hypercube
         else:
             self._im_self_supervised_loss = None
 
