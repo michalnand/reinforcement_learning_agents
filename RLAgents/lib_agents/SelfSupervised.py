b476ef3b1f16b50c1c5b3073f5f3a0801e8d8c12
michalnand <michal.nand@gmail.com>
2024-08-09 12:13:26 +0200

    ppo snd e

--- RLAgents/lib_agents/SelfSupervised.py
+++ RLAgents/lib_agents/SelfSupervised.py
@@ -170,7 +170,7 @@ def loss_vicreg_distance_categorical(model_forward_func, augmentations, x, steps
     if augmentations is not None:
         x_aug, _ = augmentations(x)
     else:
-        x_aug    = x
+        x_aug    = x    
 
     # obtain features
     z, d_pred = model_forward_func(x_aug)
@@ -194,7 +194,7 @@ def loss_vicreg_distance_categorical(model_forward_func, augmentations, x, steps
     cov_loss = _loss_cov(z)
    
     # total vicreg loss
-    loss = 1.0*dist_loss + 1.0*std_loss + (1.0/25.0)*cov_loss
+    loss = 0.0*dist_loss + 1.0*std_loss + (1.0/25.0)*cov_loss
 
     # accuracy
     acc  = (torch.argmax(d_pred, dim=1) == d_target).float()
