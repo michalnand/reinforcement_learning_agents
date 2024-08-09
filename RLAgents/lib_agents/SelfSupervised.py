b9a9b9f1d1587658b3ffaa8c3164faa65dc7827c
michalnand <michal.nand@gmail.com>
2024-08-09 17:47:28 +0200

    ppo snd e

--- RLAgents/lib_agents/SelfSupervised.py
+++ RLAgents/lib_agents/SelfSupervised.py
@@ -129,11 +129,11 @@ def loss_vicreg_distance(model_forward_func, augmentations, xa, xb, steps_a, ste
     d_target  = _target_distances(steps_a, steps_b, dist_scaling_func)
     
     # flatten predicted distances
-    d_pred    = d_pred.reshape((d_pred.shape[0]*d_pred.shape[1], d_pred.shape[2]))
+    d_pred    = d_pred.reshape((d_pred.shape[0]*d_pred.shape[1]))
     d_target  = d_target.reshape((d_target.shape[0]*d_target.shape[1]))
-    
+        
     print(">>>> ", d_target.shape, d_pred.shape)
-    
+
     # MSE loss
     dist_loss = ((d_target - d_pred)**2).mean()
 
